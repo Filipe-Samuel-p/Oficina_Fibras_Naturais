1e182be01d89fce1c5a413b014a9a92013641ad7
Filipe-Samuel-p <felipepires.p125@gmail.com>
2026-02-13 15:19:54 -0300

    feat: Creates Get product datails endpoint

--- src/main/java/oficina/fibrasnaturais/controllers/ProductController.java
+++ src/main/java/oficina/fibrasnaturais/controllers/ProductController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +25,11 @@ public class ProductController {
     public ResponseEntity<Page<ProductDTO>> getAllProduct (Pageable pageable){
         var allProducts = service.getAllProducts(pageable);
         return ResponseEntity.ok(allProducts);
+    }
+
+    @GetMapping(value = "/{productID}")
+    public ResponseEntity<ProductDTO> getProduct (@PathVariable Long productID){
+        var product = service.getProduct(productID);
+        return ResponseEntity.ok(product);
     }
 }
--- src/main/java/oficina/fibrasnaturais/services/ProductService.java
+++ src/main/java/oficina/fibrasnaturais/services/ProductService.java
@@ -1,6 +1,7 @@
 package oficina.fibrasnaturais.services;
 
 import oficina.fibrasnaturais.DTOs.product.ProductDTO;
+import oficina.fibrasnaturais.exceptions.ResourceNotFoundException;
 import oficina.fibrasnaturais.repositories.ProductRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -19,5 +20,16 @@ public class ProductService {
     public Page<ProductDTO> getAllProducts (Pageable pageable){
         var allProducts = repository.findAll(pageable);
         return allProducts.map(ProductDTO::new);
+    }
+
+
+    public ProductDTO getProduct (Long productID) {
+
+        var product = repository.findById(productID)
+                .orElseThrow(() -> new ResourceNotFoundException("Producto não encontrado"));
+
+        return new ProductDTO(product);
+
+
     }
 }
