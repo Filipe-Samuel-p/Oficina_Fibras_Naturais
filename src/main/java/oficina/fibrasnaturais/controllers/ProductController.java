983218b7da36b1cd0df2ba30d7e8875112cb7dbe
Filipe-Samuel-p <felipepires.p125@gmail.com>
2026-02-13 15:55:51 -0300

    feat: Creates update product endpoint

--- src/main/java/oficina/fibrasnaturais/controllers/ProductController.java
+++ src/main/java/oficina/fibrasnaturais/controllers/ProductController.java
@@ -6,10 +6,11 @@ import oficina.fibrasnaturais.services.ProductService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RequestMapping(value = "api/v1/product")
 @RestController
@@ -32,4 +33,22 @@ public class ProductController {
         var product = service.getProduct(productID);
         return ResponseEntity.ok(product);
     }
+
+    @PostMapping
+    @PreAuthorize("hasAllRoles('ADMIN')")
+    public ResponseEntity<ProductDTO> createProduct (@RequestBody ProductDTO dto){
+        dto = service.createProduct(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+    @PutMapping(value = "/{productID}")
+    @PreAuthorize("hasAllRoles('ADMIN')")
+    public ResponseEntity<ProductDTO> updateProduct (@RequestBody ProductDTO dto, @PathVariable Long productID){
+
+        dto = service.updateProduct(dto,productID);
+        return ResponseEntity.ok(dto);
+    }
 }
+
--- src/main/java/oficina/fibrasnaturais/services/ProductService.java
+++ src/main/java/oficina/fibrasnaturais/services/ProductService.java
@@ -1,6 +1,9 @@
 package oficina.fibrasnaturais.services;
 
 import oficina.fibrasnaturais.DTOs.product.ProductDTO;
+import oficina.fibrasnaturais.DTOs.user.AddressDTO;
+import oficina.fibrasnaturais.entities.Address;
+import oficina.fibrasnaturais.entities.Product;
 import oficina.fibrasnaturais.exceptions.ResourceNotFoundException;
 import oficina.fibrasnaturais.repositories.ProductRepository;
 import org.springframework.data.domain.Page;
@@ -30,6 +33,60 @@ public class ProductService {
 
         return new ProductDTO(product);
 
+    }
 
+    public ProductDTO createProduct (ProductDTO dto){
+
+        var newProduct = new Product();
+        dtoProductToEntity(dto,newProduct);
+        newProduct = repository.save(newProduct);
+        return new ProductDTO(newProduct);
+
+    }
+
+    public ProductDTO updateProduct (ProductDTO dto,Long productID){
+
+        var product = repository.findById(productID)
+                .orElseThrow(() -> new ResourceNotFoundException("Producto não encontrado"));
+
+
+        if (dto.getName() != null) {
+            product.setName(dto.getName());
+        }
+
+        if (dto.getDescription() != null) {
+            product.setDescription(dto.getDescription());
+        }
+
+        if (dto.getPrice() != null) {
+            product.setPrice(dto.getPrice());
+        }
+
+        if (dto.getStockQuantity() != null) {
+            product.setStockQuantity(dto.getStockQuantity());
+        }
+
+        if (dto.getImageUrl() != null) {
+            product.setImageUrl(dto.getImageUrl());
+        }
+
+        if (dto.getActive() != null) {
+            product.setActive(dto.getActive());
+        }
+
+        var updatedProduct = repository.save(product);
+
+        return new ProductDTO(updatedProduct);
+
+
+    }
+
+    private void dtoProductToEntity(ProductDTO dto, Product entity){
+       entity.setName(dto.getName());
+       entity.setDescription(dto.getDescription());
+       entity.setPrice(dto.getPrice());
+       entity.setImageUrl(dto.getImageUrl());
+       entity.setStockQuantity(dto.getStockQuantity());
+       entity.setActive(dto.getActive());
     }
 }
