0f6b8bc38fffa2658f11cf85a3b30589bce39d56
Filipe-Samuel-p <felipepires.p125@gmail.com>
2026-03-01 12:43:27 -0300

    feat: Connects the project to GCP Storage

---
+++ src/main/java/oficina/fibrasnaturais/controllers/UploadController.java
@@ -0,0 +1,32 @@
+package oficina.fibrasnaturais.controllers;
+
+import oficina.fibrasnaturais.services.ImageUploadService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/upload")
+@PreAuthorize("hasAnyRole('ADMIN', 'COORDINATOR')")
+public class UploadController {
+
+    private final ImageUploadService uploadService;
+
+    public UploadController(ImageUploadService uploadService) {
+        this.uploadService = uploadService;
+    }
+
+    @PostMapping("/image")
+    public ResponseEntity<Map<String, String>> uploadImage(@RequestParam("file") MultipartFile file) {
+
+        String imageUrl = uploadService.uploadImage(file);
+
+        return ResponseEntity.ok(Map.of("imageUrl", imageUrl));
+    }
+}
